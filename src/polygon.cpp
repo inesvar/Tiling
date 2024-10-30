efdb45e86b84efab0e7f114bc1d89df7f9eda7dd
inesvar <90340779+inesvar@users.noreply.github.com>
2024-10-30 16:08:15 +0100

    Create Polygon class

--- src/polygon.cpp
+++ src/polygon.cpp
@@ -1,17 +1,35 @@
 #include "polygon.h"
 #include <GL/glut.h>
+#include <cassert>
 #include <glm/glm.hpp>
 #include <glm/gtc/type_ptr.hpp>
+#include <iostream>
+
+using namespace glm;
+
+Polygon polygon{3};
+
+Polygon::Polygon(int nbSides) {
+    assert(nbSides >= 3);
+    points.reserve(nbSides);
+    vec2 xy = vec2(-0.0f);
+    for (int i = 0; i < nbSides; i++) {
+        points.push_back(xy);
+        xy += vec2(cos(2.0 * i * pi<double>() / nbSides),
+                   sin(2.0 * i * pi<double>() / nbSides));
+    }
+}
+
+void Polygon::render(void) const {
+    glBegin(GL_POLYGON);
+    for (const auto& vertex : points) {
+        glVertex3fv(value_ptr(vertex));
+    }
+    glEnd();
+}
 
 void displayPolygon(void) {
     glClear(GL_COLOR_BUFFER_BIT);
-    glBegin(GL_POLYGON);
-    glm::vec3 thing = glm::vec3(0.0f);
-    glVertex3fv(glm::value_ptr(thing));
-    glVertex3f(0.5, 0.0, 0.0);
-    glVertex3f(0.5, 0.5, 0.0);
-    glVertex3f(0.25, 0.625, 0.0);
-    glVertex3f(0.0, 0.5, 0.0);
-    glEnd();
+    polygon.render();
     glFlush();
 }
--- src/polygon.h
+++ src/polygon.h
@@ -1,7 +1,16 @@
 #ifndef POLYGON_H
 #define POLYGON_H
 
-class Polygon {};
+#include <glm/vec2.hpp>
+#include <vector>
+
+class Polygon {
+    std::vector<glm::vec2> points{};
+
+  public:
+    Polygon(int nbSides);
+    void render(void) const;
+};
 
 void displayPolygon(void);
 
