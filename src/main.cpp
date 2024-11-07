c5432fc37394bf57864ae6abb74c940e74702b0c
inesvar <90340779+inesvar@users.noreply.github.com>
2024-11-07 21:11:42 +0100

    Use GLAD to load GL in GLFW window

--- src/main.cpp
+++ src/main.cpp
@@ -1,22 +1,42 @@
 #include "config.h"
 #include "polygon.h"
+#include <glad/glad.h>
+#define GLFW_INCLUDE_NONE
 #include <GLFW/glfw3.h>
+#include <iostream>
 #include <string>
 
+const int WINDOW_WIDTH = 600;
+const int WINDOW_HEIGHT = 600;
+
 std::string getWindowTitle();
+void framebufferSizeCallback(GLFWwindow* window, int height, int width);
 
 int main() {
     if (!glfwInit()) {
+        std::cout << "Failed to initialize GLFW" << std::endl;
         return -1;
     }
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 
-    GLFWwindow* window = glfwCreateWindow(500, 500, getWindowTitle().data(), NULL, NULL);
+    GLFWwindow* window = glfwCreateWindow(WINDOW_WIDTH, WINDOW_HEIGHT,
+                                          getWindowTitle().data(), NULL, NULL);
+
     if (!window) {
+        std::cout << "Failed to create window" << std::endl;
         glfwTerminate();
         return -1;
     }
 
     glfwMakeContextCurrent(window);
+    if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress)) {
+        std::cout << "Failed to load GLAD" << std::endl;
+        return -1;
+    }
+    glViewport(0, 0, WINDOW_WIDTH, WINDOW_HEIGHT);
+    glfwSetFramebufferSizeCallback(window, framebufferSizeCallback);
 
     while (!glfwWindowShouldClose(window)) {
         glClear(GL_COLOR_BUFFER_BIT);
@@ -30,4 +50,10 @@ int main() {
 std::string getWindowTitle() {
     return "Tiling V" + std::to_string(VERSION_MAJOR) + "." +
            std::to_string(VERSION_MINOR);
+}
+
+void framebufferSizeCallback(__attribute__((unused)) GLFWwindow* window,
+                             int height, int width) {
+    // NOTE: this probably only works with one window...
+    glViewport(0, 0, width, height);
 }
